58e27e7de330489505794da6013c2ad817ebd1ef
Hot Stuff <hotstuff@Hots-MacBook.local>
2020-05-20 14:42:26 -0700

    update cpu.py & ls8.py

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,46 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        # initialize Register
+        self.reg = [0] * 8
+        self.ref[7] = 0xF4
+        
+        # initialize Memory
+        self.ram = [0] * 256
+        
+        # Initialize internal registers
+        self.pc = 0
+        self.ir = 0
+        self.mar = 0
+        self.mdr = 0
+        self.fl = 0
+        
+        # Initialize operation values
+        self.operand_a = 0
+        self.operand_b = 0
+        
+        # Initialize halt at false. CPU does not start halted
+        self.halt = False
+        
+        # Initialize pc_override for when pc is manually set by function
+        self.pc_override = False
+        
+        # Initialize branch_table
+        self.branch_table = {}
+        self.branch_table[0b10000010] = self.LDI
+        self.branch_table[0b01000111] = self.PRN
+        self.branch_table[0b00000001] = self.HLT
+        self.branch_table[0b01000101] = self.PUSH
+        self.branch_table[0b01000110] = self.POP
+        self.branch_table[0b01010000] = self.CALL
+        self.branch_table[0b00010001] = self.RET
+        
+        # Initialize alu_table
+        self.alu_table = {}
+        self.alu_table[0b10100010] = "MUL"
+        self.alu_table[0b10100000] = "ADD"
+        self.alu_table[0b10101000] = "AND"
+        self.alu_table[0b10100111] = "CMP"
 
     def load(self):
         """Load a program into memory."""
@@ -16,18 +55,18 @@ class CPU:
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+#         program = [
+#             # From print8.ls8
+#             0b10000010, # LDI R0,8
+#             0b00000000,
+#             0b00001000,
+#             0b01000111, # PRN R0
+#             0b00000000,
+#             0b00000001, # HLT
+#         ]
 
         for instruction in program:
-            self.ram[address] = instruction
+            self.ram_write(address, instruction)
             address += 1
 
 
@@ -36,7 +75,19 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "AND":
+            self.reg[reg_a] &= self.reg[reg_b]
+        elif op == "CMP":
+            if reg_a == reg_b:
+                self.fl = 0b00000001
+            elif reg_a > reg_b:
+                self.fl = 0b00000010
+            else:
+                self.fl = 0b00000100
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -59,7 +110,106 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+        
+    def ram_read(self, address):
+        """
+        Function that reads from memory. Takes in the address to read.
+        Returns the value stored at that address in the RAM
+        """
+        self.mar = address
+        self.mdr = self.ram[self.mar]
+        
+        return self.mdr
+    
+    def ram_write(self, address, value):
+        """
+        Function that writes to memory. Takes in the value to write,
+        the address to write to. Saves value at the given address.
+        """
+        self.mar = address
+        self.ram[self.mar] = value
+        self.mdr = self.ram[self.mar]
 
     def run(self):
         """Run the CPU."""
-        pass
+        while not self.halt:
+            self.ir = self.ram_read(self.pc + 1)
+            self.operand_a = self.ram_read(self.pc + 1)
+            self.operand_b = self.ram_read(self.pc + 2)
+            self.trace()
+            
+            # Reset pc_override to check if PC needs to be adjusted
+            self.pc_override = False
+            
+            # Determine if alu operation or branch_table operation
+            bit_5 = (self.ir & 2 ** 5) >> 5
+            
+            if bit_5:
+                op = self.alu_table[self.ir]
+                self.alu(op, self.operand_a, self.operand_b)
+                
+            if not bit_5:
+                op = self.branch_table[self.ir]()
+                
+            if not self.pc_override:
+                bit_6 = (self.ir & 2 ** 6) >> 6
+                bit_7 = (self.ir & 2 ** 7) >> 7
+                
+                if bit_6:
+                    self.pc += 2
+                if bit_7:
+                    self.pc += 3
+                    
+        sys.exit()
+        
+        
+    def LDI(self):
+        """
+        Set register to this value
+        """
+        self.reg[self.operand_a] = self.operand_b
+        
+    def PRN(self):
+        """
+        Prints numeric values stored at register's address
+        """
+        print(self.reg[self.operand_a])
+        
+    def HLT(self):
+        """
+        Sets halt value to true
+        """
+        self.halt = True
+        
+    def PUSH(self, value=None):
+        """
+        Pushes value at given register on to computer stack
+        """
+        self.reg[7] -= 1
+        if value is None:
+            value = self.reg[self.operand_a]
+        self.ram_write(self.reg[7], value)
+        
+    def POP(self, register=True):
+        """
+        Pops value at current stack pointer off the stack
+        and stores it at the given register
+        """
+        value = self.ram_read(self.reg[7])
+        if register:
+            self.reg[self.operand_a] = value
+        else:
+            return value
+        self.reg[7] += 1
+        
+    def CALL(self):
+        """
+        Calls a subroutine(function) stored at the address in the register.
+        The address of the instruction directly after CALL is pushed onto the stack.
+        The PC is set to the address stored in the given register
+        """
+        self.pc_override = True
+        self.PUSH(self.pc + 2)
+        self.pc = self.reg[self.operand_a]
+        
+#    
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,11 +1,23 @@
-#!/usr/bin/env  python3
+#!/usr/bin/env python3
 
 """Main."""
 
 import sys
 from cpu import *
 
+file = open(sys.argv[1], "r")
+
+program = []
+
+for line in file:
+    line = line.split(' ')[0]
+    line = line.split('#')[0]
+
 cpu = CPU()
 
-cpu.load()
+if len(sys.argv) != 2:
+    print("ERROR: must have file name")
+    sys.exit(1)
+
+cpu.load(sys.argv[1])
 cpu.run()
